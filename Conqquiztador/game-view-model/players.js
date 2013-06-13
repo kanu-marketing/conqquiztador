1cb6991c71411af56fb79796c47ae2bdf1cdbc0e
vencislav.staykov@gmail.com <vencislav.staykov@gmail.com@437a05c8-5701-22e1-647c-9370e75d7ff1>
2013-06-13 08:32:50 +0000

    Data hiding;

--- Conqquiztador/game-view-model/players.js
+++ Conqquiztador/game-view-model/players.js
@@ -3,30 +3,37 @@
 
 (function ($) {
     var DUMMY_PLAYER_NAMES = ["Shrek", "Fionna", "Donald", "Bugs", "Spongebob"];
+
     this.Player = Class.create({
         initialize: function (name) {
-            this.name = name;
-            this.points = 0;
-            this.flags = [];
+            this._name = name;
+            this._points = 0;
+            this._flags = [];
         },
         addFlag: function (flag) {
-            this.flags.push(flag);
+            this._flags.push(flag);
+        },
+        getName: function () {
+            return this._name;
+        },
+        getPoints: function () {
+            return this._points;
         },
         render: function () {
             var container = $("<div></div>");
             var playerId = $("<div class='player_id'></div>");
-            playerId.append("<div class='name'>" + this.name + "</div>")
-            playerId.append("<div class='points'>" + this.points + "</div>")
+            playerId.append("<div class='name'>" + this._name + "</div>")
+            playerId.append("<div class='points'>" + this._points + "</div>")
             container.append(playerId);
 
-            if (this.flags.length !== 0) {
+            if (this._flags.length !== 0) {
                 var flagsContainer = $("<p class='player_flags'></p>");
-               
-                for (var i = 0, len = this.flags.length; i < len; i += 1) {
-                    flagsContainer.append("<span>" + this.flags[i] + "; </span>");
+
+                for (var i = 0, len = this._flags.length; i < len; i += 1) {
+                    flagsContainer.append("<span>" + this._flags[i] + "; </span>");
                     // TODO: change after flags implementation
                 }
-                
+
                 container.append(flagsContainer);
             }
 
@@ -44,8 +51,11 @@
             var answer = Math.floor((Math.random() * 10 + 1) % 4);
             return answer + 1;
         },
-        getShortQuestionAnswer: function (downLimit, upLimit) {
-            var answer = 0;
+        getShortQuestionAnswer: function (question) {
+            var downLimit = question._downLimit;
+            var upLimit = question._upLimit;
+            var answer = Math.floor(Math.random() * (upLimit - downLimit + 1)) + downLimit;
+
             return answer;
         }
     });
