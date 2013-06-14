4435303a8b99c3e185a7f59be1149da1479f2012
a.s.todorova@gmail.com <a.s.todorova@gmail.com@437a05c8-5701-22e1-647c-9370e75d7ff1>
2013-06-14 05:54:52 +0000

    Correction of flags functionality

--- Conqquiztador/game-view-model/game-field.js
+++ Conqquiztador/game-view-model/game-field.js
@@ -8,31 +8,37 @@
 
         },
         startNewGame: function () {
-            $("#mouse_pointer").empty();
+            var container = $("#mouse_pointer");
+            container.empty();
+            container.append("<ul>");
+
             for (var i = 0; i < 10; i++) {
+
+                container.append("li");
                 var fieldItem = $("<img>").attr({
                     id: "flag" + i,
                     class: 'flags' ,
                     src: 'images/blue_flag.png' ,
-                    alt: 'Blue Flag' 
+                    alt: 'Blue Flag',
                 });
-                $("#mouse_pointer").append(fieldItem);
+                container.append(fieldItem);
+                container.append("</li>");
             }
+            container.append("/ul");
 
-            for (var i = 0; i < 10; i++) {
-                var redItem = $("<img>").attr({
-                    id: "red_flag" + i,
-                    class: 'flags',
-                    src: 'images/red_flag.png',
-                    alt: 'Red Flag',
-                });
-                redItem.hide();
-                $("#mouse_pointer").append(redItem);
-            }
+            //for (var i = 0; i < 10; i++) {
+            //    var redItem = $("<img>").attr({
+            //        id: "red_flag" + i,
+            //        class: 'flags',
+            //        src: 'images/red_flag.png',
+            //        alt: 'Red Flag',
+            //    });
+            //    redItem.hide();
+            //    $("#mouse_pointer").append(redItem);
+            //}
         },
         stopGame: function () {
             $("#mouse_pointer").empty();
-            //$("#question_box").empty();
         }
     });
 
@@ -42,18 +48,18 @@
 }(jQuery));
 
 
-function getPointCoordinates(event) {
+//function getPointCoordinates(event) {
 
-    pointX = event.offsetX ? (event.offsetX) : event.pageX - document.getElementById("mouse_pointer").offsetLeft;
-    pointY = event.offsetY ? (event.offsetY) : event.pageY - document.getElementById("mouse_pointer").offsetTop;
-    document.getElementById("flag").style.left = (pointX - 1);
-    document.getElementById("flag").style.top = (pointY - 15);
-    document.getElementById("flag").style.visibility = "visible";
+//    pointX = event.offsetX ? (event.offsetX) : event.pageX - document.getElementById("mouse_pointer").offsetLeft;
+//    pointY = event.offsetY ? (event.offsetY) : event.pageY - document.getElementById("mouse_pointer").offsetTop;
+//    document.getElementById("flag").style.left = (pointX - 1);
+//    document.getElementById("flag").style.top = (pointY - 15);
+//    document.getElementById("flag").style.visibility = "visible";
 
-    var coordinates = new Array;
-    coordinates[0] = pointX;
-    coordinates[1] = pointY;
-    console.log(coordinates[0], coordinates[1]);
+//    var coordinates = new Array;
+//    coordinates[0] = pointX;
+//    coordinates[1] = pointY;
+//    console.log(coordinates[0], coordinates[1]);
 
-    return coordinates;
-}
+//    return coordinates;
+//}
